84a9b0d1511956fca9b8d5a95328419468fdf871
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-01-04 15:12:35 -0500

    added some more GUI examples (drawing, animation, download progress)

---
+++ gui-stuff/green-shoes-gui/animation.rb
@@ -0,0 +1,22 @@
+
+require 'green_shoes'
+
+Shoes.app do
+  fill blue
+  @gravity = 0.05
+  @velocity = 0.0
+  @ball_radius = 60
+  @ball = oval(top: 10,
+              left: width / 2 - @ball_radius,
+              radius: @ball_radius)
+  button("Drop ball") do
+    animate(60) do
+      @velocity += @gravity
+      @ball.top += @velocity
+      if @ball.top > height - @ball_radius * 2
+        @ball.top = height - @ball_radius * 2
+        @velocity = @velocity * -0.8
+      end
+    end
+  end
+end
---
+++ gui-stuff/green-shoes-gui/drawing.rb
@@ -0,0 +1,14 @@
+
+require 'green_shoes'
+
+Shoes.app do
+  background darkgray
+  fill darkblue
+  arrow 10, 10, 100
+  fill red
+  rect 10, 100, 100
+  fill yellow
+  oval 10, 200, 50
+  strokewidth(2)
+  line 10, 300, 100, 300
+end
