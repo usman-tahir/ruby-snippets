e32870ae83c8ed8a80cbfaf62806b96ff876b67a
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-02-07 20:19:51 -0500

    added the first 10 exercises from Learn Ruby The Hard Way

---
+++ learn-ruby/ex2.rb
@@ -0,0 +1,8 @@
+# A comment, this is so you can read your program later
+# Anything after the '#' is ignored by ruby
+
+puts "I could have code like this." # And the comment after is ignored
+
+# puts "This won't run."
+
+puts "This wil run."
