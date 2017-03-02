5c4c4b74b58dcd60011f0c87ef0f9014017650c5
usman-tahir <tahir.usman.ali94@outlook.com>
2017-03-01 20:48:25 -0500

    added an image directory; rendered a game window with a sample background

--- gosu-examples/window/window.rb
+++ gosu-examples/window/window.rb
@@ -2,14 +2,16 @@ require 'gosu'
 
 class GameWindow < Gosu::Window
   def initialize
-    super(640, 480)
+    super(250, 250)
     self.caption = 'Gosu Window Example'
+    @background_image = Gosu::Image.new('./img/bricks.png')
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
 end
 
