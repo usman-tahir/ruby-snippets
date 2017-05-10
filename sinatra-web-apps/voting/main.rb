ac1130923759705c2af4ec96c17b71a3f280c5b2
usman-tahir <tahir.usman.ali94@outlook.com>
2017-05-10 16:42:24 -0400

    updated the index.erb file with some templating; added the cast/ route to handle vote casts (along with cast.erb)

--- sinatra-web-apps/voting/main.rb
+++ sinatra-web-apps/voting/main.rb
@@ -2,12 +2,20 @@
 require "sinatra"
 
 Choices = {
-    "HAM" => "Hamburger",
-    "PIZ" => "Pizza",
-    "CUR" => "Curry",
-    "NOO" => "Noodles"
+  "HAM" => "Hamburger",
+  "PIZ" => "Pizza",
+  "CUR" => "Curry",
+  "NOO" => "Noodles"
 }
 
 get "/" do
-    erb :index
+  @title = "Welcome to the Voting App"
+  erb :index
+end
+
+# Casting a vote
+post "/cast" do
+  @title = "Thank you for casting your vote!"
+  @vote = params["vote"]
+  erb :cast
 end
