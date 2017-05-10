03800fc8e099c152a25cf2f8d23a7f2b138fc63b
usman-tahir <tahir.usman.ali94@outlook.com>
2017-05-10 19:46:42 -0400

    added new functionality for the cast route (WIP: results page breaking due to output)

--- sinatra-web-apps/voting/main.rb
+++ sinatra-web-apps/voting/main.rb
@@ -1,5 +1,6 @@
 
 require "sinatra"
+require "yaml/store"
 
 Choices = {
   "HAM" => "Hamburger",
@@ -17,5 +18,20 @@ end
 post "/cast" do
   @title = "Thank you for casting your vote!"
   @vote = params["vote"]
+  @store = YAML::Store.new "votes.yml"
+  @store.transaction do
+    @store["votes"] ||= {}
+    @store["votes"][@vote] ||= 0
+    @store["votes"][@vote] += 1
+  end
   erb :cast
+end
+
+get "/results" do
+  @title = "Results so far:"
+  @store = YAML::Store.new "votes.yml"
+  @votes = @store.transaction {
+    @store["votes"]
+  }
+  erb :results
 end
